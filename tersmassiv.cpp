aff59d0a7b4cf6472aa3a3387e3c5b20017e710f
Ali İbrahimov <79019033+Thebrhmly@users.noreply.github.com>
2021-05-05 15:57:05 +0400

    Update tersmassiv.cpp

--- tersmassiv.cpp
+++ tersmassiv.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 int main()
 {
-    int a[10
-	], i,n=sizeof(a)/sizeof(int);
-    cout<<n<<" element daxil edin ";
-    for(i=0; i<n; i++)
+    int a[10], i, n=sizeof(a)/sizeof(int);
+     cout<<n<<" element daxil edin ";
+       for(i=0; i<n; i++)
         cin>>a[i];
-    cout<<"Massivin daxil edilen elementleri:  ";
-    for(i=0; i<n; i++)
-        cout<<a[i]<<" ";
-    cout<<endl<<"Massivin tersine cevrilmis elemntleri:  ";
-    for(i=(n-1); i>=0; i--)
-        cout<<a[i]<<" ";
-    cout<<endl;
+     cout<<"Massivin daxil edilen elementleri:  ";
+       for(i=0; i<n; i++)
+     cout<<a[i]<<" ";
+     cout<<endl<<"Massivin tersine cevrilmis elemntleri:  ";
+       for(i=(n-1); i>=0; i--)
+     cout<<a[i]<<" ";
+     cout<<endl;
     return 0;
 }
